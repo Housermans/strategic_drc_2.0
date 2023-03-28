da880c805028f973c86c1009aa13d1ddd54ee0b5
Housermans <mahuismans@gmail.com>
2023-03-28 14:26:01 +0200

    Script3 aangepast om numerieke variabelen te plotten

--- script/Script3-plot_functions.R
+++ script/Script3-plot_functions.R
@@ -34,8 +34,25 @@ read_plot_data <- function(exp_id, organoid_name) {
   organoid_data
 }
 
-# organoids = unique(d$org_name)
-# conditions = unique(d$condition)
+select_files_and_add_metadata <- function(select_on = "Analyse", plot_condition = "all", selection_value = 1, metadata_fac=c("chemo_naive", "RASTRIC", "Passed_QC", "Tween_bad", "DMSO_bad", "Clin_benefit", "Analyse", "Analyse2"), metadata_num=c("RAS_pct_change")) {
+  if(select_on == "all") {
+    Analyse <- overview %>% filter(Data_processed == 1)
+  } else {
+    Analyse <- overview %>% filter(get(select_on) == selection_value)
+  }
+  status <- Analyse %>% dplyr::select(STR_ID, org_name, all_of(metadata_fac), all_of(metadata_num))
+  d <- read_plot_data(Analyse[1, "STR_ID"], Analyse[1, "org_name"])
+  for (n in 2:nrow(Analyse)) {
+    r <- read_plot_data(Analyse[n, "STR_ID"], Analyse[n, "org_name"])
+    d <- rbind(d, r)
+  }
+  d <- left_join(d, status, by=c("STR_ID", "org_name"))
+  d <- d %>% mutate(across(all_of(metadata_fac), .fns = ~factor(.,levels = c(0,1), labels = c("no", "yes"))))
+  if (!plot_condition == "all") {
+    d <- d %>% filter(condition == plot_condition)
+  } 
+  d
+}
 
 plot_per_condition <- function(df, condition) {
   df <- df[df$condition == condition, ]
@@ -101,7 +118,7 @@ plot_per_condition <- function(df, condition) {
     theme_classic() +
     geom_line(data = dataframe_fit, aes(x = 10^Concentration_1, y = y_fit_original, color = org_name)) +
     scale_color_manual(values = hex_codes1) + # use your custom colors here
-    labs (x= "Concentration (log10) uM", y="GR", main = condition, color = "Organoid") +  
+    labs (x= "Concentration (log10) μM", y="GR", main = condition, color = "Organoid") +  
     scale_x_log10(limits = c(min(organoid_data$conc_condition),max(organoid_data$conc_condition))) +
     ylim(-1,1.5) + ggtitle(condition)
   return(p)
@@ -111,7 +128,7 @@ plot_per_condition_factorial <- function(df, condition, colorby="chemo_naive") {
   df <- df[df$condition == condition, ]
   title <- condition
   
-  n1 <- length(unlist(unique(df[colorby])))               # Amount of default colors
+  in1 <- length(unlist(unique(df[colorby])))               # Amount of default colors
   hex_codes1 <- hue_pal()(n1)                             # Identify hex codes
   
   # create an empty list to store the nplr models
@@ -152,8 +169,11 @@ plot_per_condition_factorial <- function(df, condition, colorby="chemo_naive") {
     colnames(dataframe_fit_i) <- c("Concentration_1", "y_fit")
     df_filtered <- filter(df, org_name == names(fit_list)[i])
     # selection_var <- df_filtered["chemo_naive"][[1]][1]
+    
+    
     selection_vec <- df_filtered %>% pull(get(colorby))
     selection_var <- selection_vec[1]
+    
     
     min_organoid = min(df[df$org_name == names(fit_list)[i], ]$mean_GR)
     max_organoid = max(df[df$org_name == names(fit_list)[i], ]$mean_GR)
@@ -174,82 +194,145 @@ plot_per_condition_factorial <- function(df, condition, colorby="chemo_naive") {
   p <- ggplot() + 
     geom_point(data = df, aes(conc_condition, mean_GR, group=org_name, color = get(colorby))) +
     geom_line(data = dataframe_fit, aes(x = 10^Concentration_1, y = y_fit_original, group=org_name, color = colorby)) +
-    scale_color_manual(values = hex_codes1) + # use your custom colors here
-    labs (x= "Concentration (log10) uM", y="GR", main = condition, color = colorby) +  
+    labs (x= "Concentration (log10) μM", y="GR", main = condition, color = colorby) +  
     scale_x_log10(limits = c(min(organoid_data$conc_condition),max(organoid_data$conc_condition))) +
     theme_classic() +
     ylim(-1,1.5) + 
     ggtitle(condition)
+  
   return(p)
 }
 
-select_files_and_add_metadata <- function(select_on = "Analyse", plot_condition = "all", selection_value = 1, metadata=c("chemo_naive", "RASTRIC", "Passed_QC", "Tween_bad", "DMSO_bad", "RASTRIC_SD", "Analyse", "Analyse2")) {
-  if(select_on == "all") {
-    Analyse <- overview %>% filter(Data_processed == 1)
-  } else {
-    Analyse <- overview %>% filter(get(select_on) == selection_value)
+plot_per_condition_numeric <- function(df, condition, colorby="RAS_pct_change") {
+  df <- df[df$condition == condition, ]
+  title <- condition
+  
+  # create an empty list to store the nplr models
+  fit_list <- list()
+  
+  # loop over the unique organoids in the condition
+  for (organoid in unique(df$org_name)) {
+    print(paste("plotting", organoid, "on", condition, "by", colorby))
+    organoid_data = df[df$org_name == organoid, ]
+    organoid_data$Max_Concentration_log <- log10(organoid_data$conc_condition)
+    
+    min_organoid = min(organoid_data$mean_GR)
+    max_organoid = max(organoid_data$mean_GR)
+    diff_organoid = max_organoid - min_organoid
+    organoid_data$GR_prop <- convertToProp(organoid_data$mean_GR)
+    
+    # fit the nplr model and store it in the list
+    fit <- nplr(organoid_data$conc_condition, organoid_data$GR_prop, 
+                useLog = TRUE,
+                LPweight = 0.25,
+                npars = "all",
+                method = "res",
+                silent = FALSE)
+    
+    fit_list[[organoid]] <- fit
+    
   }
-  status <- Analyse %>% dplyr::select(STR_ID, org_name, all_of(metadata))
-  d <- read_plot_data(Analyse[1, "STR_ID"], Analyse[1, "org_name"])
-  for (n in 2:nrow(Analyse)) {
-    r <- read_plot_data(Analyse[n, "STR_ID"], Analyse[n, "org_name"])
-    d <- rbind(d, r)
+  
+  # create an empty data frame to store the fitted values
+  dataframe_fit <- data.frame()
+  
+  # loop over the nplr models and extract the fitted values
+  for (i in seq_along(fit_list)) {
+    
+    fit <- fit_list[[i]]
+    
+    dataframe_fit_i <- data.frame(getXcurve(fit), getYcurve(fit))
+    colnames(dataframe_fit_i) <- c("Concentration_1", "y_fit")
+    df_filtered <- filter(df, org_name == names(fit_list)[i])
+    
+    selection_vec <- df_filtered %>% pull(get(colorby))
+    selection_var <- selection_vec[1]
+    
+    min_organoid = min(df[df$org_name == names(fit_list)[i], ]$mean_GR)
+    max_organoid = max(df[df$org_name == names(fit_list)[i], ]$mean_GR)
+    diff_organoid = max_organoid - min_organoid
+    
+    dataframe_fit_i$y_fit_original <- min_organoid + dataframe_fit_i$y_fit * diff_organoid
+    
+    dataframe_fit_i$condition <- condition
+    dataframe_fit_i$org_name <- names(fit_list)[i]
+    dataframe_fit_i$colorby <- selection_var
+    
+    # append the data frame to the main one
+    dataframe_fit <- rbind(dataframe_fit, dataframe_fit_i)
+    
   }
-  d <- left_join(d, status, by=c("STR_ID", "org_name"))
-  d <- d %>% mutate(across(all_of(metadata), .fns = ~factor(.,levels = c(0,1), labels = c("no", "yes"))))
-  if (!plot_condition == "all") {
-    d <- d %>% filter(condition == plot_condition)
-  } 
-  # d$chemo_naive <- as.factor(d$chemo_naive)
-  # levels(d$chemo_naive) <- c("no", "yes")
-  # d$RASTRIC <- as.factor(d$RASTRIC)
-  # levels(d$RASTRIC) <- c("no", "yes")
-  d
+  
+  # dataframe_fit$color_column <- dataframe_fit[[colorby]]
+  # Convert 'colorby' column to numeric
+  # df$colorby <- as.numeric(df$colorby)
+            
+  
+  
+  # plot all the organoids with different colors based on their org_name
+  p <- ggplot() + 
+    geom_point(data = df, aes(conc_condition, mean_GR, group=org_name, color = get(colorby))) +
+    geom_line(data = dataframe_fit, aes(x = 10^Concentration_1, y = y_fit_original, group=org_name, color = colorby)) +
+    labs (x= "Concentration (log10) μM", y="GR", main = condition, color = colorby) +  
+    scale_color_continuous(low="green", high= "red") +
+    scale_x_log10(limits = c(min(organoid_data$conc_condition),max(organoid_data$conc_condition))) +
+    theme_classic() +
+    ylim(-1,1.5) + 
+    ggtitle(condition)
+  
+  
+  
+  return(p)
 }
-# d <- select_files_and_add_metadata(select_on = "all")
 
-
-# TODO: plot_Tween=FALSE, plot_DMSO=FALSE, om specifiek de Tween/DMSO plotjes te maken.
-plot_multiple <- function(exp_name, select_on="Analyse", selection_value=1, colorby="chemo_naive") {
+plot_multiple <- function(exp_name, select_on="Analyse", selection_value=1, colorby="chemo_naive", color_numeric=FALSE) {
   if (!dir.exists(file.path(plot_output, exp_name))) {
     dir.create(file.path(plot_output, exp_name))
   }
   d <- select_files_and_add_metadata(select_on = select_on)
   for (condition in unique(d$condition)) {
     p <- plot_per_condition(d, condition)
-    q <- plot_per_condition_factorial(d, condition, colorby=colorby)
+    if (color_numeric) {
+      q <- plot_per_condition_numeric(d, condition, colorby=colorby)
+    } else {
+      q <- plot_per_condition_factorial(d, condition, colorby=colorby)
+    }
     p_and_q <- p + q
     ggsave(file.path(plot_output, exp_name, paste0(condition,"_", exp_name, "_plots.png")), p_and_q, width=4200, height=2100, units="px")
   }
 }
 
-plot_single <- function(exp_name, condition, select_on="Analyse", selection_value=1, colorby="chemo_naive") {
+plot_single <- function(exp_name, condition, select_on="Analyse", selection_value=1, colorby="chemo_naive", color_numeric=FALSE) {
   if (!dir.exists(file.path(plot_output, exp_name))) {
     dir.create(file.path(plot_output, exp_name))
   }
   d <- select_files_and_add_metadata(select_on = select_on, plot_condition = condition, selection_value = selection_value)
   p <- plot_per_condition(d, condition)
-  q <- plot_per_condition_factorial(d, condition, colorby=colorby)
+  if (color_numeric) {
+    q <- plot_per_condition_numeric(d, condition, colorby=colorby)
+  } else {
+    q <- plot_per_condition_factorial(d, condition, colorby=colorby)
+  }
   p_and_q <- p + q
   ggsave(file.path(plot_output, exp_name, paste0(condition,"_", exp_name, "_plots.png")), p_and_q, width=4200, height=2100, units="px")
 }
 
-
-
-plot_selected_conditions <- function(exp_name, select_on="Passed_QC", selection_value = 1, colorby="chemo_naive", selected_conditions=c("5-FU", "Oxaliplatin")) {
+plot_selected_conditions <- function(exp_name, select_on="Passed_QC", selection_value = 1, colorby="chemo_naive", selected_conditions=c("5-FU", "Oxaliplatin"), color_numeric=FALSE) {
   for (condition in selected_conditions) {
-    plot_single(exp_name = exp_name, condition = condition, select_on=select_on, selection_value=selection_value, colorby=colorby)
+    plot_single(exp_name = exp_name, condition = condition, select_on=select_on, selection_value=selection_value, colorby=colorby, color_numeric=color_numeric)
   }
 }
 
-dont_plot_selected_conditions <- function(exp_name, select_on="Passed_QC", selection_value = 1, colorby="chemo_naive",  unselected_conditions=c("5-FU", "Oxaliplatin")) {
+dont_plot_selected_conditions <- function(exp_name, select_on="Passed_QC", selection_value = 1, colorby="chemo_naive",  unselected_conditions=c("5-FU", "Oxaliplatin"), color_numeric=FALSE) {
   d <- select_files_and_add_metadata(select_on = select_on, selection_value = selection_value)
   for (condition in unique(d$condition)) {
     if (!condition %in% unselected_conditions) {
-      plot_single(exp_name = exp_name, condition = condition, select_on=select_on, selection_value=selection_value, colorby=colorby)
+      plot_single(exp_name = exp_name, condition = condition, select_on=select_on, selection_value=selection_value, colorby=colorby, color_numeric=color_numeric)
     }
   }
 }
+
+plot_selected_conditions("RASTRIC_pct", select_on="Analyse", selected_conditions=c("Lapatinib", "Binimetinib", "Vinorelbine", "binimetinib_lapatinib", "vi_bi_la"), colorby="RAS_pct_change", color_numeric=TRUE)
 
 # plot_multiple("STR-D2")
 # plot_multiple("plot_qc", select_on="Passed_QC")
@@ -258,8 +341,9 @@ dont_plot_selected_conditions <- function(exp_name, select_on="Passed_QC", selec
 
 
 # These function plot only those that have been checked to have good tween and dmso controls:
-plot_selected_conditions("WNT_selected", select_on="Analyse2")
-plot_selected_conditions("WNT_selected", select_on="Analyse", selected_conditions=c("SN-38", "Lapatinib", "Binimetinib", "CHEK1", "vi_bi_la"))
+# plot_selected_conditions("RASTRIC_pct", select_on="Analyse", selected_conditions=c("Lapatinib", "Binimetinib", "Vinorelbine", "binimetinib_lapatinib", "vi_bi_la"), colorby="RAS_pct_change")
+
+# plot_selected_conditions("WNT_selected", select_on="Analyse", selected_conditions=c("SN-38", "Lapatinib", "Binimetinib", "CHEK1", "vi_bi_la"))
 # plot_dmso("Good_tween_dmso")
 
 
